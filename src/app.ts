e8ada6422f2d419ceefbfe0eb103fad32ae85fd8
Paluffel <40839103+Paluffel@users.noreply.github.com>
2019-08-05 20:20:35 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -70,9 +70,9 @@ export default class HelloWorld {
                   //@ts-ignore
                 buttonBehavior.onClick((user: User) => {        const libraryActors: Array<MRE.ForwardPromise<MRE.Actor>> = [];
                     libraryActors.push(MRE.Actor.CreateFromLibrary(this.context, {
-                        resourceId: "artifact: 1265485061176689661",
+                        resourceId: "artifact: 1265485056982385660",
                         actor: {
-                            name: 'Pandafog 02',
+                            name: 'Pandafog 03',
                             transform: {
                             
                             }
